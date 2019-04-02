fc3729834bbf811ea1e796bd7c6b85fb070ac154
Sredeption <hjw_redeption@outlook.com>
2019-04-02 22:05:47 +0800

    skip list node and random height

--- src/ConcurrentSkipList.cc
+++ src/ConcurrentSkipList.cc
@@ -1,5 +1,177 @@
+#include <cassert>
 #include "ConcurrentSkipList.h"
 
 namespace Gungnir {
 
+ConcurrentSkipList::Node::Node(std::allocator<std::atomic<ConcurrentSkipList::Node *>> &allocator, uint8_t height,
+                               Key key, bool isHead)
+    : allocator(allocator), flags(), height(height), key(std::forward<Key>(key)), spinLock("SkipList::Node") {
+    setFlags(0);
+    if (isHead) {
+        setIsHeadNode();
+    }
+    // need to explicitly init the dynamic atomic pointer array
+    forward = allocator.allocate(height);
+    for (int i = 0; i < height; i++) {
+        allocator.construct(forward + i, nullptr);
+    }
+}
+
+ConcurrentSkipList::Node::~Node() {
+    allocator.deallocate(forward, height);
+}
+
+// copy the head node to a new head node assuming lock acquired
+ConcurrentSkipList::Node *ConcurrentSkipList::Node::copyHead(Node *node) {
+    assert(node != nullptr && height > node->height);
+    setFlags(node->getFlags());
+    for (uint8_t i = 0; i < node->height; ++i) {
+        setSkip(i, node->skip(i));
+    }
+    return this;
+}
+
+ConcurrentSkipList::Node *ConcurrentSkipList::Node::skip(int layer) const {
+    assert(layer < height);
+    return forward[layer].load(std::memory_order_consume);
+}
+
+// next valid node as in the linked list
+ConcurrentSkipList::Node *ConcurrentSkipList::Node::next() {
+    Node *node;
+    for (node = skip(0); (node != nullptr && node->markedForRemoval());
+         node = node->skip(0)) {
+    }
+    return node;
+}
+
+void ConcurrentSkipList::Node::setSkip(uint8_t h, Node *next) {
+    assert(h < height);
+    forward[h].store(next, std::memory_order_release);
+}
+
+Key &ConcurrentSkipList::Node::getKey() {
+    return key;
+}
+
+const Key &ConcurrentSkipList::Node::getKey() const {
+    return key;
+}
+
+int ConcurrentSkipList::Node::maxLayer() const {
+    return height - 1;
+}
+
+int ConcurrentSkipList::Node::getHeight() const {
+    return height;
+}
+
+std::unique_lock<SpinLock> ConcurrentSkipList::Node::acquireGuard() {
+    return std::unique_lock<SpinLock>(spinLock);
+}
+
+bool ConcurrentSkipList::Node::fullyLinked() const {
+    return getFlags() & FULLY_LINKED;
+}
+
+bool ConcurrentSkipList::Node::isHeadNode() const {
+    return getFlags() & IS_HEAD_NODE;
+}
+
+void ConcurrentSkipList::Node::setIsHeadNode() {
+    setFlags(uint16_t(getFlags() | IS_HEAD_NODE));
+}
+
+void ConcurrentSkipList::Node::setFullyLinked() {
+    setFlags(uint16_t(getFlags() | FULLY_LINKED));
+}
+
+void ConcurrentSkipList::Node::setMarkedForRemoval() {
+    setFlags(uint16_t(getFlags() | MARKED_FOR_REMOVAL));
+}
+
+ConcurrentSkipList::RandomHeight *ConcurrentSkipList::RandomHeight::instance() {
+    static RandomHeight instance_;
+    return &instance_;
+}
+
+int ConcurrentSkipList::RandomHeight::getHeight(int maxHeight) const {
+    assert(maxHeight < MaxHeightLimit);
+    double p = randomProb();
+    for (int i = 0; i < maxHeight; ++i) {
+        if (p < lookupTable[i]) {
+            return i + 1;
+        }
+    }
+    return maxHeight;
+}
+
+size_t ConcurrentSkipList::RandomHeight::getSizeLimit(int height) const {
+    assert(height < MaxHeightLimit);
+    return sizeLimitTable[height];
+}
+
+ConcurrentSkipList::RandomHeight::RandomHeight() : lookupTable(), sizeLimitTable() {
+    initLookupTable();
+}
+
+void ConcurrentSkipList::RandomHeight::initLookupTable() {
+    // set skip prob = 1/E
+    static const double probInv = std::exp(1);
+    static const double prob = 1.0 / probInv;
+    static const size_t maxSizeLimit = std::numeric_limits<size_t>::max();
+
+    double sizeLimit = 1;
+    double p = lookupTable[0] = (1 - prob);
+    sizeLimitTable[0] = 1;
+    for (int i = 1; i < MaxHeightLimit - 1; ++i) {
+        p *= prob;
+        sizeLimit *= probInv;
+        lookupTable[i] = lookupTable[i - 1] + p;
+        sizeLimitTable[i] = sizeLimit > maxSizeLimit
+                            ? maxSizeLimit
+                            : static_cast<size_t>(sizeLimit);
+    }
+    lookupTable[MaxHeightLimit - 1] = 1;
+    sizeLimitTable[MaxHeightLimit - 1] = maxSizeLimit;
+}
+
+double ConcurrentSkipList::RandomHeight::randomProb() {
+    static thread_local boost::lagged_fibonacci2281 rng_;
+    return rng_();
+}
+
+bool ConcurrentSkipList::greater(const Key &data, const Node *node) {
+    return node;
+}
+
+bool ConcurrentSkipList::less(const Key &data, const Node *node) {
+    return node == nullptr;
+}
+
+int ConcurrentSkipList::findInsertionPoint(Node *currentNode, int currentLayer, const Key &key,
+                                           Node *predecessors[], Node *successors[]) {
+
+    int foundLayer = -1;
+    Node *predecessor = currentNode;
+    Node *foundNode = nullptr;
+    for (int layer = currentLayer; layer >= 0; --layer) {
+        Node *node = predecessor->skip(layer);
+        while (greater(key, node)) {
+            predecessor = node;
+            node = node->skip(layer);
+        }
+        if (foundLayer == -1 && !less(key, node)) { // the two keys equal
+            foundLayer = layer;
+            foundNode = node;
+        }
+        predecessors[layer] = predecessor;
+
+        // if found, succs[0..foundLayer] need to point to the cached foundNode,
+        // as foundNode might be deleted at the same time thus pred->skip() can
+        // return nullptr or another node.
+        successors[layer] = foundNode ? foundNode : node;
+    }
+    return foundLayer;
+}
 }
--- src/ConcurrentSkipList.h
+++ src/ConcurrentSkipList.h
@@ -2,9 +2,16 @@
 #define GUNGNIR_CONCURRENTSKIPLIST_H
 
 #include <cstdint>
+#include <atomic>
+#include <memory>
+#include <cmath>
+
+
+#include <boost/random.hpp>
 
 #include "Key.h"
 #include "Value.h"
+#include "SpinLock.h"
 
 
 namespace Gungnir {
@@ -20,13 +27,108 @@ private:
         };
 
     public:
+        Node(std::allocator<std::atomic<Node *>> &allocator, uint8_t height, Key key, bool isHead);
+
+        ~Node();
+
         Node(const Node &) = delete;
 
         Node &operator=(const Node &) = delete;
 
+        Node *copyHead(Node *node);
+
+        Node *skip(int layer) const;
+
+        Node *next();
+
+        void setSkip(uint8_t h, Node *next);
+
+        Key &getKey();
+
+        const Key &getKey() const;
+
+        int maxLayer() const;
+
+        int getHeight() const;
+
+        std::unique_lock<SpinLock> acquireGuard();
+
+        bool fullyLinked() const;
+
+        bool markedForRemoval() const {
+            return getFlags() & MARKED_FOR_REMOVAL;
+        }
+
+        bool isHeadNode() const;
+
+        void setIsHeadNode();
+
+        void setFullyLinked();
+
+        void setMarkedForRemoval();
+
+    private:
+
+        uint16_t getFlags() const {
+            return flags.load(std::memory_order_consume);
+        }
+
+        void setFlags(uint16_t flags) {
+            this->flags.store(flags, std::memory_order_release);
+        }
+
+        std::allocator<std::atomic<Node *>> &allocator;
+        Key key;
+        Value value;
+        std::atomic<uint16_t> flags;
+        const uint8_t height;
+        SpinLock spinLock;
+        std::atomic<Node *> *forward;
+
     };
 
+    class RandomHeight {
+    private:
+        enum {
+            MaxHeightLimit = 64
+        };
+    public:
+        static RandomHeight *instance();
+
+        int getHeight(int maxHeight) const;
+
+        size_t getSizeLimit(int height) const;
+
+    private:
+        RandomHeight();
+
+        void initLookupTable();
+
+        static double randomProb();
+
+        double lookupTable[MaxHeightLimit];
+        size_t sizeLimitTable[MaxHeightLimit];
+    };
+
+    static bool greater(const Key &data, const Node *node);
+
+    static bool less(const Key &data, const Node *node);
+
+    static int findInsertionPoint(
+        Node *currentNode, int currentLayer, const Key &key,
+        Node *predecessors[], Node *successors[]);
+
 public:
+
+
+    class Iterator {
+
+    };
+
+    class Accessor {
+    public:
+
+    };
 
 
 };
